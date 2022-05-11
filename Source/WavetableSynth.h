171cd538239a1e40c00e2d6361405472ae229064
EnrcDamn <enrico.damiani10@gmail.com>
2022-05-11 12:01:39 +0200

    Inherit Sound and Voice classes

--- Source/WavetableSynth.h
+++ Source/WavetableSynth.h
@@ -13,6 +13,28 @@
 #include <vector>
 #include "Wavetable.h"
 
+class SynthSound : public juce::SynthesiserSound
+{
+public:
+    bool appliesToNote(int midiNoteNumber) override { return true; }
+    bool appliesToChannel(int midiChannel) override { return true; }
+};
+
+//=====================================================================
+
+class SynthVoice : public juce::SynthesiserVoice
+{
+public:
+    bool canPlaySound(juce::SynthesiserSound* sound) override;
+    void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
+    void stopNote(float velocity, bool allowTailOff) override;
+    void controllerMoved(int controllerNumber, int newControllerValue) override;
+    void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
+
+};
+
+//=====================================================================
+
 class WavetableSynth
 {
 public:
