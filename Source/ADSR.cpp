06901a92895cfad14e97a8071ae0b3abe4421d50
EnrcDamn <enrico.damiani10@gmail.com>
2022-05-13 22:10:30 +0200

    ADSR Class

--- Source/ADSR.cpp
+++ Source/ADSR.cpp
@@ -3,9 +3,91 @@
 
     ADSR.cpp
     Created: 13 May 2022 6:22:16pm
-    Author:  enric
+    Author:  Enrico Damiani
 
   ==============================================================================
 */
 
 #include "ADSR.h"
+
+
+double ADSREnv::adsr(double input, int trigger) 
+{
+    if (trigger == 1 && attackPhase != 1 && holdPhase != 1 && decayPhase != 1) 
+    {
+        holdCount = 0;
+        decayPhase = 0;
+        sustainPhase = 0;
+        releasePhase = 0;
+        attackPhase = 1;
+    }
+
+    if (attackPhase == 1) 
+    {
+        releasePhase = 0;
+        amplitude += (1 * attack);
+        output = input * amplitude;
+
+        if (amplitude >= 1) 
+        {
+            amplitude = 1;
+            attackPhase = 0;
+            decayPhase = 1;
+        }
+    }
+
+
+    if (decayPhase == 1) 
+    {
+        output = input * (amplitude *= decay);
+        if (amplitude <= sustain) {
+            decayPhase = 0;
+            holdPhase = 1;
+        }
+    }
+
+    if (holdCount < holdTime && holdPhase == 1) 
+    {
+        output = input * amplitude;
+        holdCount++;
+    }
+
+    if (holdCount >= holdTime && trigger == 1) 
+    {
+        output = input * amplitude;
+    }
+
+    if (holdCount >= holdTime && trigger != 1) 
+    {
+        holdPhase = 0;
+        releasePhase = 1;
+    }
+
+    if (releasePhase == 1 && amplitude > 0.) 
+    {
+        output = input * (amplitude *= release);
+    }
+
+    return output;
+}
+
+
+void ADSREnv::setAttack(double attackMS) 
+{
+
+}
+
+void ADSREnv::setRelease(double releaseMS) 
+{
+
+}
+
+void ADSREnv::setSustain(double sustainL) 
+{
+    
+}
+
+void ADSREnv::setDecay(double decayMS) 
+{
+
+}
--- Source/ADSR.h
+++ Source/ADSR.h
@@ -3,9 +3,30 @@
 
     ADSR.h
     Created: 13 May 2022 6:22:16pm
-    Author:  enric
+    Author:  Enrico Damiani
 
   ==============================================================================
 */
 
 #pragma once
+
+class ADSREnv {
+
+public:
+    double adsr(double input, int trigger);
+    double input;
+    double output;
+    double attack;
+    double decay;
+    double sustain;
+    double release;
+    double amplitude;
+    void setAttack(double attackMS);
+    void setDecay(double decayMS);
+    void setSustain(double sustainL);
+    void setRelease(double releaseMS);
+    int trigger;
+    long holdTime = 1;
+    long holdCount;
+    int attackPhase, decayPhase, sustainPhase, holdPhase, releasePhase;
+};
